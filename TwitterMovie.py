3e79a79fb81aaae714374cf1da274d12d6f557e1
Apple <apple@Apples-MacBook-Air.local>
2018-07-29 21:53:27 +1200

    Tokenizing

--- TwitterMovie.py
+++ TwitterMovie.py
@@ -35,6 +35,51 @@ class VoteClassifier (ClassifierI):
         for c in self._classfiers:
             v = c.classify (features)
             votes.append(v)
+            
+        choice_value = votes.count(mode(votes))
+        conf = choice_value / len (votes)
+        return conf
+    
+short_pos = open ("short_reviews/positive.txt", "r").read()
+short_neg = open ("short_reviews/negative.txt", "r").read()
+
+documents = []
+
+for k in short_pos.split('\n'):
+    documents.append(k,"pos")
+    
+for k in short_neg.split('\n'):
+    documents.append(k, "neg")
+
+
+all_words = []
+
+short_pos_words = word_tokenize(short_pos)
+short_neg_words = word_tokenize(short_neg)
+
+for w in short_pos_words:
+    all_words.append(w.lower())
+
+for w in short_neg_words:
+    all_words.append(w.lower())
+    
+    
+
+all_words = nltk.FreqDist (all_words)
+
+word_features = list (all_words.keys())[:5000]
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
